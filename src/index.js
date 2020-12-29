c6c63e1e0833877878f1abc567e81be9c6d05ef2
Andrew Rogerson <andrew.rogerson@free-now.com>
2020-12-29 18:31:55 +0100

    add restful api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import models from './models';
+import routes from './routes';
 
 const app = express();
 
@@ -10,8 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello Project');
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(port, () => console.log(`Listening on ${port}`));
